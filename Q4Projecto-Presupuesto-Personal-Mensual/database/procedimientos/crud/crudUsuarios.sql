1beb500d58cbcef6618a9ce6ee1928b47639e7cf
maria-velasquez <104383447+maria20020423@users.noreply.github.com>
2025-11-24 14:46:29 -0600

    Squashed commit of the following:
    commit 7fb39eb71ec5ab361f061803667ac54216dbe955
    Author: maria-velasquez <104383447+maria20020423@users.noreply.github.com>
    Date:   Mon Nov 24 14:45:58 2025 -0600
    
        Update crudUsuarios.sql
    
    commit e6956d6672b731cf275d8d9370940b03da2fc06c
    Author: maria-velasquez <104383447+maria20020423@users.noreply.github.com>
    Date:   Mon Nov 24 14:44:59 2025 -0600
    
        Crear CRUD de usuario

---
+++ Q4Projecto-Presupuesto-Personal-Mensual/database/procedimientos/crud/crudUsuarios.sql
@@ -0,0 +1,69 @@
+CREATE OR REPLACE PROCEDURE sp_insertar_usuario(
+	IN p_nombre varchar(50),
+	IN p_apellido varchar(50),
+	IN p_email varchar(50),
+	IN p_salario_mensual decimal(10,2), 
+	IN p_creado_por varchar(50)
+)
+INSERT INTO usuarios(
+	nombres,
+	apellidos,
+	correo_electronico,
+	salario_base,
+	creado_por,
+	modificado_por
+) VALUES (
+	p_nombre,
+	p_apellido,
+	p_email,
+	p_salario_mensual,
+	p_creado_por,
+	p_creado_por
+)
+
+CREATE OR REPLACE PROCEDURE sp_actualizar_usuario(
+	p_id_usuario integer,
+	p_nombre varchar(50),
+	p_apellido varchar(50),
+	p_salario_mensual decimal(10,2),
+	p_modificado_por varchar(50)
+)
+BEGIN 
+	UPDATE usuarios set 
+	nombres			= p_nombre,
+	apellidos		= p_apellido,
+	salario_base	= p_salario_mensual,
+	modificado_por	= p_modificado_por,
+	modificado_en	= current server timestamp
+	WHERE id_usuario= p_id_usuario;
+END;
+		
+CREATE OR REPLACE PROCEDURE sp_eliminar_usuario(p_id_usuario integer)
+BEGIN 
+	UPDATE usuarios 
+	SET es_activo = 0
+	WHERE id_usuario= p_id_usuario;
+
+END
+
+CREATE OR REPLACE PROCEDURE sp_consultar_usuario(p_id_usuario integer)
+BEGIN 
+IF NOT EXISTS (
+	SELECT 1
+	FROM usuarios
+	WHERE id_usuario = p_id_usuario
+) THEN (
+	RAISERROR 50000 'No hay usuario con esta ID'
+END IF;
+
+	SELECT * 
+	FROM usuarios 
+	WHERE id_usuario = p_id_usuario;
+END;
+
+CREATE PROCEDURE sp_listar_usuarios()
+BEGIN
+    SELECT *
+    FROM usuarios
+    ORDER BY id_usuario DESC;
+END;
