11101e663ad19272dd9e56f2dd008da7e78cdf57
Marcelo Molina <molinamarcelo113@gmail.com>
2025-12-02 13:28:05 -0600

    Update transacciones_crud.sql

--- Q4Projecto-Presupuesto-Personal-Mensual/database/procedimientos/crud/transacciones_crud.sql
+++ Q4Projecto-Presupuesto-Personal-Mensual/database/procedimientos/crud/transacciones_crud.sql
@@ -12,24 +12,25 @@ CREATE OR REPLACE PROCEDURE sp_insertar_transaccion(
 	IN p_fecha date,
 	IN p_metodo_pago varchar(50),
 	IN p_num_factura varchar(100),
-	IN p_observaciones text,
-	IN p_creado_por varchar(100)
+	IN p_comentarios_extra varchar(100),
+	IN p_creado_por varchar(50)
 )
 BEGIN
 	INSERT INTO transacciones(
 		id_usuario,
 		id_presupuesto,
-		anio,
-		mes,
+		anio_transaccion,
+		mes_transaccion,
 		id_subcategoria,
 		id_obligacion,
-		tipo,
+		tipo_transaccion,
 		descripcion,
 		monto,
-		fecha,
+		fecha_transaccion,
 		metodo_pago,
 		num_factura,
-		observaciones,
+		comentarios_extra,
+		fecha_hora_registro,
 		creado_por,
 		modificado_por
 	) VALUES (
@@ -45,7 +46,8 @@ BEGIN
 		p_fecha,
 		p_metodo_pago,
 		p_num_factura,
-		p_observaciones,
+		p_comentarios_extra,
+		CURRENT TIMESTAMP,
 		p_creado_por,
 		p_creado_por
 	);
@@ -62,19 +64,19 @@ CREATE OR REPLACE PROCEDURE sp_actualizar_transaccion(
 	IN p_fecha date,
 	IN p_metodo_pago varchar(50),
 	IN p_num_factura varchar(100),
-	IN p_observaciones text,
-	IN p_modificado_por varchar(100)
+	IN p_comentarios_extra varchar(100),
+	IN p_modificado_por varchar(50)
 )
 BEGIN
 	UPDATE transacciones SET
-		anio = p_anio,
-		mes = p_mes,
+		anio_transaccion = p_anio,
+		mes_transaccion = p_mes,
 		descripcion = p_descripcion,
 		monto = p_monto,
 		fecha = p_fecha,
 		metodo_pago = p_metodo_pago,
 		num_factura = p_num_factura,
-		observaciones = p_observaciones,
+		comentarios_extra = p_comentarios_extra,
 		modificado_por = p_modificado_por,
 		modificado_en = CURRENT TIMESTAMP
 	WHERE id_transaccion = p_id_transaccion;
@@ -122,8 +124,8 @@ BEGIN
 	SELECT *
 	FROM transacciones
 	WHERE id_presupuesto = p_id_presupuesto
-	AND (p_anio IS NULL OR anio = p_anio)
-	AND (p_mes IS NULL OR mes = p_mes)
-	AND (p_tipo IS NULL OR tipo = p_tipo)
+	AND (p_anio IS NULL OR anio_transaccion = p_anio)
+	AND (p_mes IS NULL OR mes_transaccion = p_mes)
+	AND (p_tipo IS NULL OR tipo_transaccion = p_tipo)
 	ORDER BY fecha DESC;
 END;
